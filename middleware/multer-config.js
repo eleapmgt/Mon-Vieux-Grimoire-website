dc09c20434f53ec3b8e0367df3b1aecabf554d73
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-07-23 15:40:27 +0200

    fix: error handling in multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -5,15 +5,27 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage }).single('image');
 
 module.exports = (req, res, next) => {
-  upload(req, res, () => {
-      const { buffer, originalname } = req.file;
-      const timestamp = new Date().toISOString();
-      const ref = `${timestamp}-${originalname}.webp`;
-      const path = "./images/" + ref;
-      sharp(buffer)
-        .webp({ quality: 20 })
-        .toFile(path);
-      req.file.filename = ref;
-      next();
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erreur de téléchargement du fichier'});
+    }
+
+    if (!req.file) {
+      return next();
+    }
+
+    const { buffer, originalname } = req.file;
+    const timestamp = new Date().toISOString();
+    const ref = `${timestamp}-${originalname}.webp`;
+    const path = "./images/" + ref;
+
+    sharp(buffer)
+      .webp({ quality: 20 })
+      .toFile(path)
+      .then(() => {
+        req.file.filename = ref;
+        next();
+      })
+      .catch(() => res.status(500).json({ error: 'Erreur dans le processus de traitement de l\'image'}));
   });
 };
