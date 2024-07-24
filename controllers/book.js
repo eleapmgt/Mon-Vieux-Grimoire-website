913a689f037d52e74204a2e0e228e2ab571634a6
Elea Pimouguet <elea.pimouguet@gmail.com>
2024-07-24 15:45:19 +0200

    build: bestrating route + controller

--- controllers/book.js
+++ controllers/book.js
@@ -1,6 +1,29 @@
 const Book = require('../models/Book');
 const fs = require('fs');
 
+exports.getAllBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.bestBooks = (req, res, next) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => {
+    console.log(books, 'books');
+    res.status(200).json(books);
+    })
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+  .then((book) => res.status(200).json(book))
+  .catch((error) => res.status(404).json({ error }));
+};
+
 exports.createBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
@@ -13,12 +36,6 @@ exports.createBook = (req, res, next) => {
   book.save()
     .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
     .catch((error) => res.status(400).json({ error }));
-};
-
-exports.getOneBook = (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-  .then((book) => res.status(200).json(book))
-  .catch((error) => res.status(404).json({ error }));
 };
 
 exports.modifyBook = (req, res, next) => {
@@ -58,12 +75,6 @@ exports.deleteBook = (req, res, next) => {
       }
     })
     .catch(error => res.status(500).json({ error }));
-};
-
-exports.getAllBooks = (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
 };
 
 exports.rateBook = (req, res, next) => {
--- routes/book.js
+++ routes/book.js
@@ -7,6 +7,7 @@ const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('', bookCtrl.getAllBooks);
+router.get('/bestrating', bookCtrl.bestBooks);
 router.get('/:id', bookCtrl.getOneBook);
 router.post('', auth, multer, bookCtrl.createBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
